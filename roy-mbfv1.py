8b7a9c709aee4096d2146ab4aca3812658690f35
royofficial <85897883+royofficial@users.noreply.github.com>
2021-06-19 14:46:32 +0700

    Update roy-mbfv1.py

--- roy-mbfv1.py
+++ roy-mbfv1.py
@@ -17,7 +17,7 @@ try: import requests as req
 except ModuleNotFoundError: os.system("python -m pip install requests");restart()
 try: from bs4 import BeautifulSoup as parser
 except ModuleNotFoundError: os.system("python -m pip install bs4");restart()
-ua_="Mozilla/5.0 (Linux; Android 5.1.1; SM-G600S Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36"
+ua_=s.get("https://raw.githubusercontent.com/avsid/data-anggaxd/main/ua.txt").text.strip()
 runtah=["password/__pycache__","kentod/__pycache__"]
 try:
 	hapus(runtah[0])
